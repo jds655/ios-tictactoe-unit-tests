dfdfe788f1073241eec8930700596627f78868db
Joshua Sharp <sharp.josh@gmail.com>
2019-10-09 19:10:50 -0400

    Wrote Game struct

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+
+enum GameError: Error, Equatable {
+    case noActivePlayer
+    case boardIsFull
+}
+
+struct Game {
+    private(set) var board: GameBoard
+    
+    internal var activePlayer: GameBoard.Mark?
+    internal var gameIsOver: Bool
+    internal var winningPlayer: GameBoard.Mark?
+    
+    init() {
+        activePlayer = .x
+        winningPlayer = nil
+        board = GameBoard()
+        gameIsOver = false
+    }
+    
+    mutating internal func restart() {
+        self.activePlayer = .x
+        self.winningPlayer = nil
+        self.board = GameBoard()
+        self.gameIsOver = false
+    }
+    
+    mutating internal func makeMark(at coordinate: Coordinate) throws {
+        guard let activePlayer = activePlayer else {
+            throw GameError.noActivePlayer
+        }
+        do {
+            try board.place(mark: activePlayer, on: coordinate)
+        } catch {
+            throw error
+        }
+        
+        if checkGame(board: board, isWonBy: activePlayer) {
+            self.winningPlayer = activePlayer
+            self.activePlayer = nil
+            self.gameIsOver = true
+            return
+        } else {
+            if activePlayer == .x { self.activePlayer = .o}
+            else {self.activePlayer = .x}
+            if !checkGame(board: board, isWonBy: activePlayer) && board.isFull
+            {
+                self.gameIsOver = true
+                self.winningPlayer = nil
+                self.activePlayer = nil
+                //throw GameError.boardIsFull
+            }
+        }
+    }
+    
+    
+}
